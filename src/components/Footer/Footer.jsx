642331471d2fd4942a414357b73ae77ce94b1692
Aladin Feratovic <aladin.feratovic@gmail.com>
2020-02-28 14:28:20 -0500

    Updated footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -35,10 +35,7 @@ const Footer = () => {
         </div>
         <hr />
         <p className="footer__text">
-          © {new Date().getFullYear()} - Template developed by{' '}
-          <a href="https://github.com/cobidev" target="_blank" rel="noopener noreferrer">
-            Jacobo Martínez
-          </a>
+          © {new Date().getFullYear()} - Aladin Feratovic
         </p>
 
         {isEnabled && <GithubButtons />}
